9374742a97534a02febba34ba4ff752b41cfa78d
Raisul999 <raisulislamarnob0@gmail.com>
2022-11-03 17:59:18 +0600

    changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const axios = require('axios');
+const dotenv = require('dotenv').config()
 const { ETwitterStreamEvent, TweetStream, TwitterApi, ETwitterApiError } = require('twitter-api-v2');
 const app = express()
 
@@ -9,34 +10,22 @@ app.use(express.json());
 
 const PORT = 5000;
 
-// app.get('/', (req,res)=>{
-//     res.send('Hello World!');
-// })
 
-
-    
-    
 
 const server = async () => {
-   
+
     // const { channel, keyword } = req.body
 
     // console.log(channel, keyword)
 
-    const client = new TwitterApi('AAAAAAAAAAAAAAAAAAAAACAhiwEAAAAAKYLu5uGcWXgL1zFGLNML5VjNDi4%3DM75PwdAE3ikXwXkAmm7lYesLhrR21ax5KV2CSVQMJ7hieUig2w');
+    const client = new TwitterApi(process.env.TWITTER_TOKEN);
 
 
     const stream = await client.v2.searchStream();
 
-    // const deleteRules = await client.v2.updateStreamRules({
-    //     delete: {
-    //         ids: ['1587685212011757569'],
-    //     },
-    // });
 
-    // console.log(deleteRules)
 
-    
+
     let data
     stream.on(
         // Emitted when a Twitter payload (a tweet or not, given the endpoint).
@@ -44,13 +33,13 @@ const server = async () => {
         async eventData => {
             console.log('Twitter has sent something:', eventData)
             data = eventData.data
-            await axios.post('https://n8n-training.up.railway.app/webhook-test/twitter-stream', {
+            await axios.post('https://n8n-training.up.railway.app/webhook/twitter-stream', {
                 eventData
-    })
+            })
         },
     );
 
-    
+
 }
 
 
