7076ed77b689160c280b3fb8e1c8ef80744ce541
Raisul999 <raisulislamarnob0@gmail.com>
2022-11-05 15:44:11 +0600

    env config

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const server = async () => {
 
     // console.log(channel, keyword)
 
-    console.log(process.env.TWITTER_TOKEN)
+    // console.log(process.env.TWITTER_TOKEN)
 
     const client = new TwitterApi(process.env.TWITTER_TOKEN);
 
